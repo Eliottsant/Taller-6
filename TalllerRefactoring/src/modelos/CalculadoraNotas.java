e97f15e7b82d33be0081cf6525a81cda02f6c3a8
Eliottsant <eliisant@espol.edu.ec>
2022-01-06 12:38:48 -0500

    Cambios en la clase estudiante y eliminacion de la clase informacionAdicionalProfesor

---
+++ TalllerRefactoring/src/modelos/CalculadoraNotas.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelos;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Eliot
+ */
+public class CalculadoraNotas {
+    //Calcula y devuelve la nota inicial contando examen, deberes, lecciones y talleres. El teorico y el practico se calcula por parcial.
+
+    public double CalcularNotaInicial(Paralelo p, double nexamen, double ndeberes, double nlecciones, double ntalleres, ArrayList<Paralelo> paralelos) {
+        double notaInicial = 0;
+        for (Paralelo par : paralelos) {
+            if (p.equals(par)) {
+                double notaTeorico = (nexamen + ndeberes + nlecciones) * 0.80;
+                double notaPractico = (ntalleres) * 0.20;
+                notaInicial = notaTeorico + notaPractico;
+            }
+        }
+        return notaInicial;
+    }
+
+    //Calcula y devuelve la nota final contando examen, deberes, lecciones y talleres. El teorico y el practico se calcula por parcial.
+    public double CalcularNotaFinal(Paralelo p, double nexamen, double ndeberes, double nlecciones, double ntalleres, ArrayList<Paralelo> paralelos) {
+        double notaFinal = 0;
+        for (Paralelo par : paralelos) {
+            if (p.equals(par)) {
+                double notaTeorico = (nexamen + ndeberes + nlecciones) * 0.80;
+                double notaPractico = (ntalleres) * 0.20;
+                notaFinal = notaTeorico + notaPractico;
+            }
+        }
+        return notaFinal;
+    }
+
+    //Calcula y devuelve la nota inicial contando examen, deberes, lecciones y talleres. Esta nota es solo el promedio de las dos calificaciones anteriores.
+    public double CalcularNotaTotal(Paralelo p, ArrayList<Paralelo> paralelos) {
+        double notaTotal = 0;
+        for (Paralelo par : paralelos) {
+            if (p.equals(par)) {
+                notaTotal = (p.getMateria().notaInicial + p.getMateria().notaFinal) / 2;
+
+            }
+        }
+        return notaTotal;
+
+    }
+}
